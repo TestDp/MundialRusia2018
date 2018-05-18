be9b43392d8a76bde78559bde46782f86ec4f357
diegodpsoluciones <33739109+diegodpsoluciones@users.noreply.github.com>
2018-05-17 21:08:34 -0500

    Add register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-2">
-						<div id="fh5co-logo"><a href="{{ url('/') }}"><img class="img-responsive" src="{{ asset('images/logo.jpg') }}" alt=""></a></div>
+						<div id="fh5co-logo"><a href="{{ url('/') }}"><img class="img-responsive" src="" alt=""></a></div>
 					</div>
 					<div class="col-xs-10 text-right menu-1">
 						<ul>
