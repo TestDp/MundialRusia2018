3016757cc5e3dec5a06cb28a8f3702b7414eecac
diegodpsoluciones <33739109+diegodpsoluciones@users.noreply.github.com>
2018-05-17 20:59:21 -0500

    Implementacion Vistas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,68 +8,151 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Calendario Mundialista GyJ Aceros</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Calendario Mundialista GyJ Aceros" />
+	<meta name="keywords" content="" />
+	
+		<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="{{ asset('css/flexslider.css') }}">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+	<!-- Modernizr JS -->
+	<script src="{{ asset('js/modernizr-2.6.2.min.js') }}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	
+
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+	<div class="fh5co-loader"></div>
+	
+    <div id="page">
+        <nav class="fh5co-nav" role="navigation" style="background-image:url(images/header.jpg);">
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+						<div id="fh5co-logo"><a href="{{ url('/') }}"><img class="img-responsive" src="{{ asset('images/logo.jpg') }}" alt=""></a></div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="{{ url('/') }}">Inicio</a></li>
+							<li><a href="{{ url('/') }}">Grupos</a></li>
+							<li><a href="{{ url('/') }}">Calendario</a></li>
+							<li><a href="{{ url('/') }}">Historia</a></li>
+							<li><a href="{{ url('/') }}">Sedes</a></li>
+							@if (Route::has('login'))
+                                 @auth
+							 <li class="btn-cta"><a href="{{ url('/home') }}"><span>Home</span></a></li>
+							  @else
+							<li class="btn-cta"><a href="{{ route('login') }}"><span>Iniciar Sesión</span></a></li>
+							<li class="btn-cta"><a href="{{ route('register') }}"><span>Registrarse</span></a></li>
+							@endauth
+							 @endif
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</nav>
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
+        <main class="py-4" style="background-image:url(images/gris.jpg);">
             @yield('content')
         </main>
+		
+			<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+				<img class="img-responsive" src="{{ asset('images/gyjfooter.png') }}" alt="">
+				</div>
+				<div class="col-md-4 col-md-push-1">
+					<small style="font-family: 'bold2'; font-size: 20px; color: #fff;" class="block">servicioalcliente@gyj.com.co</small> 
+				</div>
+
+				<div class="col-md-4 col-md-push-1">
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="https://www.facebook.com/GYJEmpresasdeAcero/" target="_blank"><img class="img-responsive" src="{{ asset('images/FACE.png') }}"></img></a></li>
+							<li><a href="https://twitter.com/GYJ_Oficial" target="_blank"><img class="img-responsive" src="{{ asset('images/TWITTER.png') }}"></img></a></li>
+							<li><a href="https://www.youtube.com/channel/UCIdDwkE9gqnffzXGqPkiabg" target="_blank"><img class="img-responsive" src="{{ asset('images/YOU.png') }}"></img></a></li>
+							<li><a href="https://plus.google.com/u/0/104883500440571217116" target="_blank"><img class="img-responsive" src="{{ asset('images/GOO.png') }}"></img></a></li>
+							<li><a href="#"><img class="img-responsive" src="{{ asset('images/LI.png') }}"></img></a></li>
+						</ul>
+					</p>
+				</div>
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+						<small style="font-family: 'bold2'; font-size: 20px; color: #fff;" class="block">Tel: 018000 126812</small> 
+						<small style="font-family: 'bold2'; font-size: 20px; color: #fff;" class="block">Bogotá: 261 5890</small>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+		
     </div>
+	
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
+	<!-- jQuery Easing -->
+	<script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+	<!-- Bootstrap -->
+	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+	<!-- Waypoints -->
+	<script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+	<!-- Stellar Parallax -->
+	<script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
+	<!-- Carousel -->
+	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+	<!-- Flexslider -->
+	<script src="{{ asset('js/jquery.flexslider-min.js') }}"></script>
+	<!-- countTo -->
+	<script src="{{ asset('js/jquery.countTo.js') }}"></script>
+	<!-- Magnific Popup -->
+	<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ asset('js/magnific-popup-options.js') }}"></script>
+	<!-- Main -->
+	<script src="{{ asset('js/main.js') }}"></script>
+	
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,170 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <title>Calendario Mundialista GyJ Aceros</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Calendario Mundialista GyJ Aceros" />
+	<meta name="keywords" content="" />
+	
+		<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
 
-            .full-height {
-                height: 100vh;
-            }
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="{{ asset('css/flexslider.css') }}">
 
-            .position-ref {
-                position: relative;
-            }
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+	<!-- Modernizr JS -->
+	<script src="{{ asset('js/modernizr-2.6.2.min.js') }}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
+</head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+	<div class="fh5co-loader"></div>
+        <div id="page">
+			<nav class="fh5co-nav" role="navigation" style="background-image:url(images/header.jpg);">
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+						<div id="fh5co-logo"><a href="index.html">Law<span>.</span></a></div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="{{ url('/') }}">Inicio</a></li>
+							<li><a href="{{ url('/') }}">Grupos</a></li>
+							<li><a href="{{ url('/') }}">Calendario</a></li>
+							<li><a href="{{ url('/') }}">Historia</a></li>
+							<li><a href="{{ url('/') }}">Sedes</a></li>
+							@if (Route::has('login'))
+                                 @auth
+							 <li class="btn-cta"><a href="{{ url('/home') }}"><span>Home</span></a></li>
+							  @else
+							<li class="btn-cta"><a href="{{ route('login') }}"><span>Iniciar Sesión</span></a></li>
+							<li class="btn-cta"><a href="{{ route('register') }}"><span>Registrarse</span></a></li>
+							@endauth
+							 @endif
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</nav>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            <div id="fh5co-started" style="background-image:url(images/img_bg_2.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<img style="display: initial !important;" class="img-responsive" src="{{ asset('images/calendario.png') }}"></img>
+					<img style="display: initial !important;" class="img-responsive" src="{{ asset('images/copa.png') }}"></img>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<img style="display: initial !important;" class="img-responsive" src="{{ asset('images/emociones.png') }}"></img>
+				</div>
+			</div>
+		</div>
+	</div>
+			
+						<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+				<img class="img-responsive" src="{{ asset('images/gyjfooter.png') }}" alt="">
+				</div>
+				<div class="col-md-4 col-md-push-1">
+					<small style="font-family: 'bold2'; font-size: 20px; color: #fff;" class="block">servicioalcliente@gyj.com.co</small> 
+				</div>
+
+				<div class="col-md-4 col-md-push-1">
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="https://www.facebook.com/GYJEmpresasdeAcero/" target="_blank"><img class="img-responsive" src="{{ asset('images/FACE.png') }}"></img></a></li>
+							<li><a href="https://twitter.com/GYJ_Oficial" target="_blank"><img class="img-responsive" src="{{ asset('images/TWITTER.png') }}"></img></a></li>
+							<li><a href="https://www.youtube.com/channel/UCIdDwkE9gqnffzXGqPkiabg" target="_blank"><img class="img-responsive" src="{{ asset('images/YOU.png') }}"></img></a></li>
+							<li><a href="https://plus.google.com/u/0/104883500440571217116" target="_blank"><img class="img-responsive" src="{{ asset('images/GOO.png') }}"></img></a></li>
+							<li><a href="#"><img class="img-responsive" src="{{ asset('images/LI.png') }}"></img></a></li>
+						</ul>
+					</p>
+				</div>
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+						<small style="font-family: 'bold2'; font-size: 20px; color: #fff;" class="block">Tel: 018000 126812</small> 
+						<small style="font-family: 'bold2'; font-size: 20px; color: #fff;" class="block">Bogotá: 261 5890</small>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+			
         </div>
+		
+			<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
+	<!-- jQuery Easing -->
+	<script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+	<!-- Bootstrap -->
+	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+	<!-- Waypoints -->
+	<script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+	<!-- Stellar Parallax -->
+	<script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
+	<!-- Carousel -->
+	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+	<!-- Flexslider -->
+	<script src="{{ asset('js/jquery.flexslider-min.js') }}"></script>
+	<!-- countTo -->
+	<script src="{{ asset('js/jquery.countTo.js') }}"></script>
+	<!-- Magnific Popup -->
+	<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ asset('js/magnific-popup-options.js') }}"></script>
+	<!-- Main -->
+	<script src="{{ asset('js/main.js') }}"></script>
+		
     </body>
 </html>
